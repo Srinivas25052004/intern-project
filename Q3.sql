8de8224e88a5e62366c3cc688c70af6cb138954e
Srinivas25052004 <151834764+Srinivas25052004@users.noreply.github.com>
2026-04-14 22:47:04 +0530

    Delete Q3.sql

--- Q3.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    MachineID,
-    Plant,
-    AVG(ProductionUnits) AS AvgProduction,
-    COUNT(CASE WHEN MaintenanceFlag = 1 THEN 1 END) AS MaintenanceCount
-FROM production_data
-GROUP BY MachineID, Plant
-ORDER BY AvgProduction ASC
-LIMIT 10;
